b6d0179924f0e11f7d7d989606cf99f9590fe57f
spookyambassador98 <bakuwork08@gmail.com>
2024-07-19 06:07:43 -0400

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3333;
+const bodyparser = require('body-parser')
+
+
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/', bodyparser.urlencoded({extended: false}))
+
+const esp32Url = 'http://10.0.0.242'; // Replace with your ESP32's local IP address
+
+app.get('/led/on', async (req, res) => {
+  try {
+    const response = await axios.get(`${esp32Url}/led/on`);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error communicating with ESP32');
+  }
+});
+
+app.get('/led/off', async (req, res) => {
+  try {
+    const response = await axios.get(`${esp32Url}/led/off`);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error communicating with ESP32');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
